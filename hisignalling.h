0adf3614630c9de89ee53390d20613348da31601
Zhen_Yu_Qian_Ye <117185259+dkbgsk@users.noreply.github.com>
2024-07-10 16:02:40 +0800

    [h

--- hisignalling.h
+++ hisignalling.h
@@ -1,17 +1,4 @@
-/*
- * Copyright (c) 2022 HiSilicon (Shanghai) Technologies CO., LIMITED.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
  
 #ifndef HISIGNALLING_H
 #define HISIGNALLING_H
@@ -24,16 +11,7 @@
 #define HISIGNALLING_MSG_ONE_FRAME_LEN      (16)
 #define HISIGNALLING_MSG_BUFF_LEN           (512)
 
-/*
- * @brief 外设扩展板和Robot板的选择
- * 使用时选择打开宏，使用外设扩展板打开#define BOARD_SELECT_IS_EXPANSION_BOARD这个宏
- * 使用Robot板打开#define BOARD_SELECT_IS_ROBOT_BOARD这个宏
- *
- * @brief Selection of peripheral expansion board and Robot board
- * Select to open the macro when using it,
- * and use the peripheral expansion board to open the #define BOARD_SELECT_IS_EXPANSION_BOARD macro
- * Use the Robot board to open the #define BOARD_SELECT_IS_ROBOT_BOARD macro
- */
+
 #define BOARD_SELECT_IS_EXPANSION_BOARD
 #ifdef BOARD_SELECT_IS_EXPANSION_BOARD
 #define EXPANSION_BOARD
